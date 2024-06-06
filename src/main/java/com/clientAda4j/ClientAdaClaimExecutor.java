32607a17920b01814106107c1efc3b873947edd3
12803 <18810243420@163.com>
2024-06-06 16:54:51 +0800

    重构

--- src/main/java/com/clientAda4j/ClientAdaClaimExecutor.java
+++ src/main/java/com/clientAda4j/ClientAdaClaimExecutor.java
@@ -1,17 +1,17 @@
-package com.clientAda4j.component;
+package com.clientAda4j;
 
-import com.clientAda4j.Executor;
+import com.clientAda4j.component.AnnotationPointCutExecutor;
 import com.clientAda4j.domain.ClientResponseProp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * 注解端点执行器
+ * 请求执行器
  *
  * @author wanghe
  */
 @Component
-public class ClientAdaRequestExecutor implements Executor {
+public class ClientAdaClaimExecutor implements Executor {
     @Autowired
     private AnnotationPointCutExecutor executor;
 
